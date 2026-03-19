92cd9a610e925bd2fa71851a6b80a2ea9a4fc105
Mohammad Shahryar Khan <shahryarsk9@gmail.com>
2026-03-19 15:53:46 +0530

    Add files via upload
    Added grade calculator program in C using if-else ladder.

---
+++ grade-calculator.c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+ int marks;
+ // Input marks
+ printf("Enter student's marks: ");
+ scanf("%d", &marks);
+ // Check grade using if-else if-else ladder
+ if (marks >= 90) {
+ printf("Grade A\n");
+ }
+ else if (marks >= 75) {
+ printf("Grade B\n");
+ }
+ else if (marks >= 60) {
+ printf("Grade C\n");
+ }
+ else if (marks >= 50) {
+ printf("Grade D\n");
+ }
+ else {
+ printf("Grade F\n");
+ }
+ return 0;
+}
